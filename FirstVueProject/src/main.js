30a832ea32ab27f5f4c7f92cc28ec7469a99f6a9
NickyWisenov <NikolaiWisenov@outlook.com>
2018-07-20 01:50:42 +1000

    third one

--- FirstVueProject/src/main.js
+++ FirstVueProject/src/main.js
@@ -1,5 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// I think this is the main root for the Vue project.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
