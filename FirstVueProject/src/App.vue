1693423951d67f8b0d5e09d8092796e987580178
NickyWisenov <NikolaiWisenov@outlook.com>
2018-07-20 16:47:04 +1000

    "Complete the Task"

--- FirstVueProject/src/App.vue
+++ FirstVueProject/src/App.vue
@@ -1,23 +1,45 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </nav>
+  <router-view />
   </div>
 </template>
 
 <script>
+import Skills from './components/Skills.vue'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Skills
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css?family=Montserrat:400,700');
+
+body {
+  background-color: #eeeeee;
+  font-family: 'Montserrat', sans-serif;
+  display: grid;
+  grid-template-rows: auto;
+  justify-items: center;
+  padding-top: 50px;
+}
+nav {
+  padding: 20px 20px 20px 0;
+}
+nav a {
+  padding: 10px;
+  text-decoration: none;
+  background: #fff;
+  border-radius: 3px;
+  color: rgb(0, 110, 255);
+  font-weight: bold;
+  margin-right: 15px;
 }
 </style>
---
+++ FirstVueProject/src/components/About.vue
@@ -0,0 +1,22 @@
+<template>
+   <div id="about">
+       <h1>Hello {{ their_name }}, This is an About Page</h1>
+       <p>This is the about page. You can easily understand what you are seeing at this page.</p>
+   </div> 
+</template>
+
+<script>
+export default {
+    name: 'About',
+    data() {
+        return {
+            their_name: this.$route.params.name
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
+
